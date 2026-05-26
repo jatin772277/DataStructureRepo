77e0e3df8170c0ecea4b681577241d39deed061a
Jatin Baghel <bt24csd025@iiitn.ac.in>
2026-05-26 21:13:28 +0530

    Added Left Rotate By d places

---
+++ 25_left_rotate_byDplaces.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+void printArray(vector<int> &arr){
+    for(int i = 0;i < arr.size();i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl<<endl;
+}
+void leftRotateD(vector<int> &arr,int d){
+    d = d % arr.size();
+    if(d == arr.size()) return;
+    reverse(arr.begin(),arr.begin() + d);
+    reverse(arr.begin()+d,arr.end());
+    reverse(arr.begin(),arr.end());
+}
+int main(){
+    vector<int> arr = {1,2,3,4,5,6,7,8,9,10,11};
+    int d = 27;
+    printArray(arr);
+    leftRotateD(arr,d);
+    printArray(arr);
+    return 0;
+}
