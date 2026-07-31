e8d5fa7717387a651fbdb1920481d0a33dda2103
Jatin Baghel <bt24csd025@iiitn.ac.in>
2026-07-31 22:37:42 +0530

    Updated 205

--- 205_longest_common_subsequence.cpp
+++ 205_longest_common_subsequence.cpp
@@ -25,8 +25,18 @@ int LongestCommonSubsequenceLengthTabulation(string &str1,string &str2){
     }
     return dp[n1][n2];
 }
-int LongestCommonSubsequenceLengthSpaceOptimized(string &str1,string& str2){
-    
+int LongestCommonSubsequenceLengthSpaceOptimized(string &str1, string &str2){
+    int n1 = str1.size();
+    int n2 = str2.size();
+    vector<int> prev(n2 + 1, 0), curr(n2 + 1, 0);
+    for(int i = 1; i <= n1; i++){
+        for(int j = 1; j <= n2; j++){
+            if(str1[i - 1] == str2[j - 1]) curr[j] = 1 + prev[j - 1];
+            else curr[j] = max(prev[j], curr[j - 1]);
+        }
+        prev = curr;
+    }
+    return prev[n2];
 }
 int main(){
     string str1 = "adcbe";
